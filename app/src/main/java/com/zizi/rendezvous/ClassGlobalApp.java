7e19751c6bfd7d34139b5c59723e50ed07164bec
BobbeYo1983 <880846@gmail.com>
2020-11-21 14:09:37 +0300

    Update ClassGlobalApp.java

--- app/src/main/java/com/zizi/rendezvous/ClassGlobalApp.java
+++ app/src/main/java/com/zizi/rendezvous/ClassGlobalApp.java
@@ -4,6 +4,14 @@ import android.app.Application;
 import android.content.SharedPreferences;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,14 +24,22 @@ import java.util.Map;
 public class ClassGlobalApp extends Application {
 
     private Map<String, String> paramsToSave; // коллекция ключ-значение
+    private Map<String, Object> mapDocument; //коллекция ключ-значение для вычитки документа из БД, будем возвращать ее
 
     // инициализация объекта работы энергонезавичимой памятью, первый параметр имя файла, второй режим доступа, только для этого приложения
     private SharedPreferences sharedPreferences; //для работы с памятью
     private SharedPreferences.Editor editorSharedPreferences; // объект для редакции энергонезависимого хранилища
 
+    // Cloud Firestore
+    private FirebaseFirestore firebaseFirestore; // база данных
+    private DocumentReference documentReference; // ссылка на документ
+
+
     public ClassGlobalApp(){
 
         paramsToSave = new HashMap<>(); // коллекция ключ-значение
+        mapDocument = new HashMap<String, Object>();
+        firebaseFirestore = FirebaseFirestore.getInstance(); //инициализация БД
 
     }
 
@@ -66,7 +82,9 @@ public class ClassGlobalApp extends Application {
 
     }
 
-
+    /**
+     * Сохраняет параметры в БД после предварительной подготовки методом PreparingToSave()
+     */
     public void SaveParams () {
 
         //editorSharedPreferences = sharedPreferences.edit(); // подготавливаем редактор работы с памятью перед записью
@@ -81,10 +99,50 @@ public class ClassGlobalApp extends Application {
         paramsToSave.clear(); //чистим буфер для параметров перед следующей записью
     }
 
+    /**
+     * Читает параметр из памяти телефона
+     * @param paramName имя параметра
+     * @return возвращает значение параметра в виде String
+     */
     public String GetParam (String paramName) {
 
         return sharedPreferences.getString(paramName, "");
 
+    }
+
+    /**
+     * Читает документ из базы Firebase Firestore
+     * @param nameCollection имя коллекции
+     * @param nameDocument имя документа
+     * @return коллекцию "ключ-значение"
+     */
+    public Map<String, Object> ReadDocument (final String nameCollection, final String nameDocument) {
+
+        mapDocument.clear(); // очищаем коллекцию
+
+        documentReference = firebaseFirestore.collection(nameCollection).document(nameDocument); // формируем путь к документу
+        documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() { // вешаем слушателя на задачу чтения документа из БД
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) { // как задача чтения выполнилась
+                if (task.isSuccessful()) { // если выполнилась успешно
+                    DocumentSnapshot document = task.getResult(); // получаем документ
+                    if (document.exists()) { // если документ такой есть, не null
+
+                        mapDocument = document.getData(); // получаем данные из документа БД
+
+                    } else { // если документа не существует
+
+                        Log(this.getClass().getSimpleName(), "ReadDocument", "Запрошенного документа (" + nameCollection + "/" + nameDocument + ") нет в БД");
+                    }
+
+                } else { // если ошибка чтения БД
+
+                    Log (this.getClass().getSimpleName(), "ReadDocument", "Ошибка чтения БД: " + task.getException());
+                }
+            }
+        });
+
+        return mapDocument;
     }
 
 
